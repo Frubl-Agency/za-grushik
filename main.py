84e70f6fca4743f49ff1fab267a997cb9cd098d3
Frubl <frubl.agency@gmail.com>
2024-06-24 17:11:20 +0200

    small style, try split mp3 with video formats

--- main.py
+++ main.py
@@ -1,28 +1,34 @@
 import yt_dlp
 from nicegui import ui, app
 from pathlib import Path
-# import webview
 
 def download_playlist(playlist_url, is_playlist, quality, format):
     ydl_opts = {
-        'format': 'bestaudio/best',
-        'extractaudio': True,  # only keep the audio
-        'audioformat': 'mp3',  # convert to mp3
-        'audioquality': '0',  # best audio quality
+        'format': 'bestvideo+bestaudio/best',  # download best video+audio format
         'outtmpl': r'D:\Music\%(title)s.%(ext)s',  # name the downloaded file
-        'yesplaylist': True,  # download entire playlist
         'ignoreerrors': True,  # continue on download errors
         'retries': float('inf'),  # retry infinitely on error
         'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '0',
-        }, {
             'key': 'FFmpegMetadata',
         }],
-        'embedthumbnail': True,  # embed thumbnail in mp3
-        'embedmetadata': True,  # embed metadata in mp3
+        'embedthumbnail': True,  # embed thumbnail if possible
+        'embedmetadata': True,  # embed metadata if possible
+        'yesplaylist': is_playlist  # download entire playlist if is_playlist is True
     }
+
+    if format == 'mp3':
+        ydl_opts.update({
+            'format': 'bestaudio/best',
+            'extractaudio': True,  # only keep the audio
+            'audioformat': format,  # convert to the chosen format
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': format,
+                'preferredquality': '0',
+            }, {
+                'key': 'FFmpegMetadata',
+            }],
+        })
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([playlist_url])
@@ -51,10 +57,11 @@ with ui.column().classes('main__inner'):
     with ui.column().classes('settings') as settings:
         with ui.row().classes('settings__item is-playlist'):
             playlist_checkbox = ui.checkbox('Download entire playlist').classes('checkbox')
-        with ui.row().classes('settings__item'):
+        with ui.row().classes('settings__item quality'):
             ui.label('Audio/Video Quality')
-            quality_slider = ui.slider(min=0, max=100, step=10, value=80).classes('range')
-            ui.label().bind_text_from(quality_slider, 'value').classes('range-slider__value')
+            with ui.row().classes('range__wrapper'):
+                quality_slider = ui.slider(min=0, max=100, step=10, value=80).classes('range')
+                ui.label().bind_text_from(quality_slider, 'value').classes('range-slider__value')
         with ui.row().classes('settings__item'):
             ui.label('Format')
             format_select = ui.select(['mp3', 'webm', 'mp4'], value='mp3').classes('media-format')
